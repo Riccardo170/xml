349894976aa0e1b9e36e335a6169305e92492d70
lai2-01 <lai2-01@itismeucci.com>
2021-12-01 11:22:20 +0100

    escompito

---
+++ src/main/java/it/galli/ClientStr.java
@@ -0,0 +1,85 @@
+package it.galli;
+
+import java.io.*;
+import java.net.*;
+
+
+public class ClientStr {
+    String nomeServer = "localhost";
+    int portaServer = 6789;
+    Socket miosocket;
+    BufferedReader tastiera;
+    String stringUtente;
+    String stringRicevutaDalServer;
+    DataOutputStream outVersoServer;
+    BufferedReader inDalServer;
+
+    public Socket connetti(){
+        System.out.println("2 CLIENT partito in esecuzione ...");
+        try
+        {
+            tastiera= new BufferedReader(new InputStreamReader(System.in));
+            miosocket = new Socket (nomeServer, portaServer);
+            outVersoServer = new DataOutputStream(miosocket.getOutputStream());
+            inDalServer = new BufferedReader(new InputStreamReader(miosocket.getInputStream()));
+            stringRicevutaDalServer=inDalServer.readLine();
+             System.out.println("server: "+stringRicevutaDalServer);
+        }
+        catch (UnknownHostException a){
+            System.err.println("Host sconosciuto");
+        }
+            catch (Exception e)
+            {
+                System.out.println(e.getMessage());
+                System.out.println("Errore durante la connessione");
+                System.exit(1);
+            }
+            return miosocket;
+     }
+
+     public void comunica(){
+         try{
+             for(;;){
+             stringRicevutaDalServer=inDalServer.readLine();
+             System.out.println("server: "+stringRicevutaDalServer);
+             stringUtente = tastiera.readLine();
+             outVersoServer.writeBytes( stringUtente+'\n');
+
+             stringRicevutaDalServer=inDalServer.readLine();
+             System.out.println("server: "+stringRicevutaDalServer);
+             stringUtente = tastiera.readLine(); 
+             outVersoServer.writeBytes( stringUtente+'\n');
+             
+             stringRicevutaDalServer=inDalServer.readLine();
+             System.out.println("server: "+stringRicevutaDalServer);
+             stringUtente = tastiera.readLine();
+             outVersoServer.writeBytes( stringUtente+'\n');
+             
+
+             stringRicevutaDalServer=inDalServer.readLine();
+             System.out.println("server: "+stringRicevutaDalServer);
+             stringRicevutaDalServer=inDalServer.readLine();
+             System.out.println("server: "+stringRicevutaDalServer);
+             stringUtente = tastiera.readLine();
+             outVersoServer.writeBytes( stringUtente+'\n');
+             
+             
+             }
+         }
+         catch (Exception e)
+         {
+             System.out.println(e.getMessage());
+             System.out.println("Errore durante la comunicazione col server!");
+             System.exit(1);
+         }
+     }
+
+     public static void main (String args[]){
+        ClientStr cliente = new ClientStr();
+        cliente.connetti();
+        cliente.comunica();
+    }
+
+
+    }
+
---
+++ src/main/java/server/ServerStr.java
@@ -0,0 +1,83 @@
+package server;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ServerStr
+{
+    ServerSocket server = null;
+    Socket client = null;
+    String operazione = null;
+    String numero1=null;
+    String numero2=null;
+    String stringaricevuta=null;
+    String stringaModificata = null;
+    int risultato=0;
+    String Risultato=null;
+
+    BufferedReader inDalClient;
+    DataOutputStream outVersoClient;
+
+
+public Socket attendi()
+{
+    try
+    {
+        System.out.println(" Server partito in esecuzione ...");
+        server = new ServerSocket(6789);
+        client = server.accept();
+        server.close();
+        inDalClient=new BufferedReader (new InputStreamReader(client.getInputStream()));
+        outVersoClient= new DataOutputStream ( client.getOutputStream());
+        outVersoClient.writeBytes("Connessione effettuata!"+'\n');
+    }
+
+    catch (Exception e)
+    {
+        System.out.println(e.getMessage());
+        System.out.println("Errore durante l'istanza del server!");
+        System.exit(1);
+    }
+    return client;
+
+}
+
+public void comunica()
+{
+    try
+    {
+        for(;;){
+             outVersoClient.writeBytes("Indovina il numero"+'\n');
+             numero1 = inDalClient.readLine();
+             System.out.println ("numero 1 : " +numero1);
+
+            int Numero1=Integer.parseInt(numero1);
+
+            
+           
+            outVersoClient.writeBytes("Vuoi effettuare un nuovo calcolo (Y/N)?"+'\n');
+            stringaricevuta=inDalClient.readLine();
+            if(stringaricevuta.equals("N")){
+                outVersoClient.writeBytes("socket in chiusura"+'\n');
+                client.close();
+                break;
+            }
+             
+        }
+    }
+    catch (Exception e)
+    {
+        System.out.println(e.getMessage());
+        System.out.println("Errore durante la comunicazione col server!");
+        System.exit(1);
+    }
+}
+
+public static void main(String args[]){
+    ServerStr servente = new ServerStr();
+    servente.attendi();
+    servente.comunica();
+}
+}
+
